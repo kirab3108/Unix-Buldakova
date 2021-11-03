491cbc60b7278d9c91176e6348ec467590b07db7
kirab3108 <92291818+kirab3108@users.noreply.github.com>
2021-11-03 14:17:27 +0300

    Update Scanner.py

--- 2_threaded_server/Scanner.py
+++ 2_threaded_server/Scanner.py
@@ -1,1 +1,18 @@
+import socket
+import tqdm
+import threading
+p = threading.Thread()
 
+ports = [] #доступные
+N = 100000
+for port in tqdm.tqdm(range(1, N + 1)):
+    sock = socket.socket()
+    try:
+        sock.connect(port)
+        ports.append(port)
+    except:
+        continue
+    finally:
+        sock.close()
+for port in ports:
+    print(port)
