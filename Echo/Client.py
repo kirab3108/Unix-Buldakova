e684f3060669452847221cb9277b50147d12ccb2
kirab3108 <92291818+kirab3108@users.noreply.github.com>
2021-11-03 13:39:49 +0300

    Update Client.py

--- Echo/Client.py
+++ Echo/Client.py
@@ -2,12 +2,12 @@ import socket
 import time
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port = int(input('Введите здесь номер порта (default is 2000)\n'))
-host = input('Введите здесь имя хоста (default is localhost)\n')
+port = int(input('Введите здесь номер порта'))
+host = input('Введите здесь имя хоста')
 try:
     client.connect((host, port))
 except (ConnectionRefusedError, socket.gaierror):
-    print("Соединение сейчас недоступно.\nХотите установить значения по умолчанию? Y\\N")
+    print("Соединение сейчас недоступно. Хотите установить значения по умолчанию? Y\\N")
     if input() == "Y":
         client.connect(('127.0.0.1', 2000))
     else:
@@ -16,18 +16,18 @@ except (ConnectionRefusedError, socket.gaierror):
 print(client.recv(2048).decode('utf-8'))
 try:
         while True:
-            client.send(input('Введите текст для сервера:\n').encode('utf-8'))
+            client.send(input('Введите текст для сервера:').encode('utf-8'))
             data = client.recv(2048).decode('utf-8')
             time.sleep(3)
 
             if data.lower() == 'exit':
-                print('>>>\nДо свидания!\n<<<')
+                print('До свидания!')
                 client.send(encode('utf-8')
                 client.shutdown(socket.SHUT_WR)
                 exit()
 
             if data:
-                print('\nData from server:')
-                print(data, end='\n\n')
+                print('Данные с сервера:')
+                print(data)
 except KeyboardInterrupt:
     client.shutdown(socket.SHUT_WR)
